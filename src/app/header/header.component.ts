693d4dea60ecc1a1d4121220560966a479e00581
Mateusz113 <mateusz.ch.113@gmail.com>
2023-11-08 12:59:43 +0100

    Update header.component.ts
    Added the logout button functionality

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Cart, CartItem } from '../models/cart.model';
-import { Item } from '../models/item.model';
-import { CartService } from '../services/cart.service';
+import {Component, Input} from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {Cart, CartItem} from '../models/cart.model';
+import {Item} from '../models/item.model';
+import {CartService} from '../services/cart.service';
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-header',
@@ -10,33 +11,44 @@ import { CartService } from '../services/cart.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  value='localhost:4200/home';
-  private _cart : Cart = {items: []};
+  value = 'localhost:4200/home';
+  private _cart: Cart = {items: []};
   itemsQuantity = 0;
+
   @Input()
-  get cart(): Cart{
+  get cart(): Cart {
     return this._cart;
   }
 
-  set cart(cart : Cart){
+  set cart(cart: Cart) {
     this._cart = cart;
 
     this.itemsQuantity = cart.items
-    .map((item) => item.quantity)
-    .reduce((prev, current) => prev + current, 0);
+      .map((item) => item.quantity)
+      .reduce((prev, current) => prev + current, 0);
   }
-  constructor(private cartService:CartService, private _snackBar: MatSnackBar){
+
+  constructor(
+    private cartService: CartService,
+    private _snackBar: MatSnackBar,
+    private authService: AuthService
+  ) {
 
   }
-  getTotal(items: Array<CartItem>):number{
+
+  getTotal(items: Array<CartItem>): number {
     return this.cartService.getTotal(items);
   }
 
-  onCopyToClipboard(){
-    this._snackBar.open('Copied to clipboard.' , 'Ok', {duration : 3000});
+  onCopyToClipboard() {
+    this._snackBar.open('Copied to clipboard.', 'Ok', {duration: 3000});
   }
 
-  onClearCart(){
+  onClearCart() {
     this.cartService.clearCart();
+  }
+
+  logout() {
+    this.authService.logout()
   }
 }
