8c572c3f1e944bd1ebb9d9ccf188d05721bbaa66
Mateusz113 <mateusz.ch.113@gmail.com>
2023-11-08 12:57:12 +0100

    Update user.model.ts
    Updates made to fit the registration and login requirements

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,4 +1,16 @@
-export interface User {
-    email: String,
-    password: String;
+export class UserRepresentation {
+  email?: string
+  password?: string
+  firstname?: string
+  lastname?: string
+  nickname?: string
+  role: string = "USER"
+
+  constructor(email?: string, password?: string, firstname?: string, lastname?: string, nickname?: string) {
+    this.email = email
+    this.password = password
+    this.firstname = firstname
+    this.lastname = lastname
+    this.nickname = nickname
+  }
 }
