33fdd3783169fa56f390768beaa252ced91b7860
Mateusz113 <mateusz.ch.113@gmail.com>
2023-11-08 13:01:23 +0100

    Update auth.service.ts
    Changed the authentication functions to meet the api endpoints and added the token related functionality

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,22 +1,73 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { shareReplay } from 'rxjs';
-import { User } from '../models/user.model';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {AuthRepresentation} from "../models/auth.model";
+import {UserRepresentation} from "../models/user.model";
+import {Router} from "@angular/router";
+import {JWTTokenService} from "./jwttoken.service";
+import {of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  API_URL = '/api/v1/auth';
-     
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private jwtTokenService: JWTTokenService
+  ) {
   }
-    
-  login(email:string, password:string ) {
-      return this.http.post<User>(this.API_URL + '/login', {email, password}).pipe(shareReplay())
-          // this is just the HTTP call, 
-          // we still need to handle the reception of the token
-          
+
+  setToken(token: string) {
+    localStorage.setItem('token', token)
+  }
+
+  getToken() {
+    return localStorage.getItem('token')
+  }
+
+  isLoggedIn() {
+    let token = this.getToken()
+    if (token != null) {
+      return !this.jwtTokenService.isTokenExpired(token)
+    } else {
+      return false
+    }
+  }
+
+  logout() {
+    localStorage.removeItem('token')
+    this.router.navigate([('login')])
+  }
+
+  login(user: UserRepresentation) {
+    return this.http.post<AuthRepresentation>('/api/v1/auth/authenticate', {
+      email: user.email!!,
+      password: user.password
+    }).pipe().subscribe({
+      next: response => {
+        if (response.token) {
+          this.setToken(response.token)
+          this.router.navigate([('/home')])
+        }
+      }
+    })
+  }
+
+  register(user: UserRepresentation) {
+    return this.http.post<AuthRepresentation>('/api/v1/auth/register', {
+      firstname: user.firstname!!,
+      lastname: user.lastname!!,
+      nickname: user.nickname!!,
+      email: user.email!!,
+      password: user.password!!,
+      role: user.role
+    }).pipe().subscribe({
+      next: response => {
+        if (response.token) {
+          this.setToken(response.token)
+          this.router.navigate([('/home')])
+        }
+      }
+    })
   }
 }
-  
