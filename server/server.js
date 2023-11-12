a75e0e4679f7792cc8abe1d6f3a4ba6b91977d84
adz011 <adrianzlotkowski011@gmail.com>
2023-11-12 15:55:02 +0100

    Stripe service added

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(cors({ origin: true, credentials: true }));
+
+const stripe = require("stripe")("sk_test_51O99oULdNiMwQpkvMEJPy5I4m1B4J7ZY2C3KAksDbZEysbSVTRLKiWdAismCAIb7O2FhaKAx63xb5Mrl3M7Rpc0u00v79wspo1");
+
+app.post("/checkout", async (req, res, next) => {
+    try {
+        const session = await stripe.checkout.sessions.create({
+            line_items:  req.body.items.map((item) => ({
+            price_data: {
+              currency: 'usd',
+              product_data: {
+                name: item.name,
+                images: [item.thumbnail]
+              },
+              unit_amount: item.price * 100,
+            },
+            quantity: item.quantity,
+          })),
+           mode: "payment",
+           success_url: "http://localhost:4242/success.html",
+           cancel_url: "http://localhost:4242/cancel.html",
+        });
+
+        res.status(200).json(session);
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.listen(4242, () => console.log('app is running on 4242'));
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component} from '@angular/core';
 import { Cart, CartItem } from '../models/cart.model';
 import { CartService } from '../services/cart.service';
+import { loadStripe } from '@stripe/stripe-js';
 
 @Component({
   selector: 'app-cart',
@@ -11,47 +12,58 @@ import { CartService } from '../services/cart.service';
 export class CartComponent {
   cart: Cart = {items: []};
 
-dataSource : Array<CartItem> = [];
+  dataSource : Array<CartItem> = [];
 
-displayedColumns: Array<string> = [
-  'product',
-  'name',
-  'price',
-  'quantity',
-  'total',
-  'action',
-]
+  displayedColumns: Array<string> = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action',
+  ]
 
-constructor(private cartService: CartService,  private http: HttpClient){
+  constructor(private cartService: CartService,  private http: HttpClient){
 
-}
+  }
 
-ngOnInit(){
-  this.cartService.cart.subscribe((_cart:Cart) =>{
-    this.cart = _cart;
-    this.dataSource = this.cart.items;
-  });
-}
-getTotal(items: Array<CartItem>):number{
-  return this.cartService.getTotal(items);
-}
-onRemoveFromCart(element:CartItem){
-  this.cartService.removeFromCart(element);
-}
+  ngOnInit(){
+    this.cartService.cart.subscribe((_cart:Cart) =>{
+      this.cart = _cart;
+      this.dataSource = this.cart.items;
+    });
+  }
 
-onClearCart(){
-  this.cartService.clearCart();
-}
-onAddQuantity(element : CartItem){
-  this.cartService.addToCart(element);
-}
-onRemoveQuantity(element : CartItem){
-  this.cartService.removeQuantity(element);
-}
+  getTotal(items: Array<CartItem>):number{
+    return this.cartService.getTotal(items);
+  }
+
+  onRemoveFromCart(element:CartItem){
+    this.cartService.removeFromCart(element);
+  }
+
+  onClearCart(){
+    this.cartService.clearCart();
+  }
+
+  onAddQuantity(element : CartItem){
+    this.cartService.addToCart(element);
+  }
+
+  onRemoveQuantity(element : CartItem){
+    this.cartService.removeQuantity(element);
+  }
 
   onCheckout(): void {
     this.http
       .post('http://localhost:4242/checkout', {
         items: this.cart.items,
       })
-}}
+      .subscribe(async (res: any) => {
+        let stripe = await loadStripe('pk_test_51O99oULdNiMwQpkv2eEt2W5pFzKB4BlME4Wn1Ud1umF126zLzsvi7UebReovqqPYXiBiA8ypty0D4urbi8CcB4Za00dEPiRg5y');
+        stripe?.redirectToCheckout({
+          sessionId: res.id,
+        });
+      });
+  }
+}
